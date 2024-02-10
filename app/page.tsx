56cdb16c70fa6ed6f8e17efa19ff205b098ff698
James-wasonga <jameswasonga22@gmail.com>
2024-02-10 21:49:19 +0300

    my commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-  <>
+  <main>
   <h1>Hello World</h1>
-  </>
+  <link href='/users'>Users</link>
+  </main>
     
   )
 }
