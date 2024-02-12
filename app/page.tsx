96a73debc4dba54bdfaf5ba4cb9f3a406606c8ef
James-wasonga <jameswasonga22@gmail.com>
2024-02-12 23:57:34 +0300

    my  commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import Image from 'next/image'
+import NewUsers from './users/new/NewUsers'
+import UsersPage from './users/page'
 
 export default function Home() {
   return (
   <main>
   <h1>Hello World</h1>
   <link href='/users'>Users</link>
+  <NewUsers/>
+  <UsersPage/>
+  <NewUsers/>
   </main>
     
   )
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
-const UsersPage = () => {
+const UsersPage = async () => {
+ interface User{
+  id:number;
+  name:string
+ }
+const res = await fetch('https://jsonplaceholder.typicode.com/users');
+const users: User[] = await res.json();
+
+
+
   return (
-    <div>This is User's Page</div>
+    <>
+     <h1>This is User's Page</h1>
+    <ul>
+    
+    {users.map(user => <li key={user.id}>{user.name}</li>)}
+    </ul>
+ 
+    </>
   )
 }
 
