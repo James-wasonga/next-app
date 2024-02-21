1947ef53c3a1a2e4818391a1153e85be733ee207
James-wasonga <jameswasonga22@gmail.com>
2024-02-21 23:38:07 +0300

    changes of some aspects

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -16,9 +16,7 @@ const users: User[] = await res.json();
      <h1>This is User's Page</h1>
     <ul>
     
-    {users.map(user => <li key={user.id}>{user.name}</li>)}
-    {users.map(user => <li key={user.name}>{user.name}</li>)}
-    {users.map(user => <li key={user.email}>{user.name}</li>)}
+    
 
 
 
